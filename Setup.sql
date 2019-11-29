c1f3d4d7fe0a085b5b48ad9cc27903a72b062bc8
Thomas Gnandt <thomas.gnandt@mayflower.de>
2019-11-29 15:46:18 +0100

    working with hardcoded table

---
+++ Setup.sql
@@ -0,0 +1,57 @@
+CREATE TYPE postgraphile_audit_plugin.audit_event AS ( id BIGINT,
+audit_id BIGINT,
+event_id INT,
+transaction_id INT,
+stmt_date TIMESTAMPTZ,
+session_info jsonb,
+values_before jsonb,
+values_after jsonb);
+
+CREATE OR REPLACE FUNCTION postgraphile_audit_plugin.get_audit_information (
+_audit_id BIGINT,
+_schema_name text,
+_table_name text
+) RETURNS SETOF postgraphile_audit_plugin.audit_event AS $$
+WITH audits AS (
+SELECT
+	rl.id,
+	rl.audit_id,
+	rl.event_id,
+	tr.id AS transaction_id,
+	tr.stmt_date,
+	COALESCE(tr.session_info,
+	'{}'::JSONB) AS session_info,
+	rl.changes AS values_before
+FROM
+	pgmemento.transaction_log tr
+JOIN pgmemento.table_event_log te ON
+	tr.id = te.transaction_id
+JOIN pgmemento.row_log rl ON
+	te.id = rl.event_id )
+SELECT
+	*,
+	(
+	SELECT
+		values_after - 'audit_id' AS values_after
+	FROM
+		pgmemento.restore_record
+		(
+			CASE WHEN values_before IS NULL THEN 1
+			ELSE audits.event_id::INT
+	END,
+		audits.event_id::INT + 1,
+		_table_name,
+		_schema_name,
+		_audit_id,
+		TRUE) AS (values_after JSONB))
+FROM
+	audits
+WHERE
+	audits.audit_id = _audit_id
+	
+	$$ Language SQL stable;
+	
+	
+	
+	
+	
--- src/index.ts
+++ src/index.ts
@@ -18,30 +18,8 @@ export default makeExtendSchemaPlugin(build => {
           audit: AuditEventsConnection @pgQuery(
             source: ${embed(
               (queryBuilder: QueryBuilder, args: AuditEventArgs) => {
-
                 return sql.fragment`
-(WITH single_row_logs as (
-	SELECT * FROM (
-		(SELECT 1 AS event_id, ${queryBuilder.getTableAlias()}.audit_id)
-		UNION ALL
-		(select event_id, audit_id from row_log where audit_id = ${queryBuilder.getTableAlias()}.audit_id)
-	) t
-	WHERE event_id >= ${sql.value(args.after || 0)}
-	LIMIT ${sql.value(args.first)} + 1
-),
-boundaries AS (
-	SELECT DISTINCT MIN(event_id) "from", MAX(event_id) "to" FROM single_row_logs
-)
- SELECT
- log->'audit_id' AS audit_id,
-
- log->'event_id' AS event_id,
-  log->'transaction_id' AS transaction_id,
-  log - 'audit_id' - 'event_id' - 'transaction_id'
-FROM
-	boundaries,
-  pgmemento.restore_records(boundaries."from", boundaries."to"+1, 'users', 'postgraphile_audit_plugin', 1, true)
-   AS (log JSONB))
+                postgraphile_audit_plugin.get_audit_information(${queryBuilder.getTableAlias()}.audit_id, 'postgraphile_audit_plugin', 'users')
             `;
               }
             )}

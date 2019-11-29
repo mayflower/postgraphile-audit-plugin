2c86b19cbe6d9cadd92555fe17118fc182fd6c1f
Lenz Weber <lenz.weber@mayflower.de>
2019-11-29 16:33:29 +0100

    apply on all applicable classes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
 import { makeExtendSchemaPlugin, gql, embed } from "graphile-utils";
-import { QueryBuilder } from "graphile-build-pg";
-
-interface AuditEventArgs {
-  first: number;
-  last: number;
-  offset: number;
-  before: string;
-  after: string;
-}
+type DocumentNode = import("graphql").DocumentNode;
 
 export default makeExtendSchemaPlugin(build => {
   const sql: typeof import("pg-sql2") = build.pgSql;
+  const inflection = build.inflection;
+  const pgClasses: import("graphile-build-pg").PgClass[] =
+    build.pgIntrospectionResultsByKind.class;
+  const auditedClasses = pgClasses.filter(
+    pgClass =>
+      pgClass.namespaceName !== "pgmemento" &&
+      pgClass.classKind === "r" &&
+      pgClass.attributes.some(pgAttribute => pgAttribute.name === "audit_id")
+  );
+
+  const typeDefs = auditedClasses.reduce<DocumentNode>(
+    (acc: null | DocumentNode, pgClass): DocumentNode => {
+      let typeDef = gql`
+      extend type ${inflection.tableType(pgClass)} {
+        audit: AuditEventsConnection @pgQuery(
+          source: ${embed(
+            (
+              queryBuilder: import("graphile-build-pg").QueryBuilder
+            ) => sql.fragment`
+              postgraphile_audit_plugin.get_audit_information(
+                ${queryBuilder.getTableAlias()}.audit_id, 
+                ${sql.value(pgClass.namespaceName)}, 
+                ${sql.value(pgClass.name)})`
+          )}
+        )
+      }
+    `;
+      if (acc === null) {
+        return typeDef;
+      } else {
+        (acc.definitions as any) = [...acc.definitions, ...typeDef.definitions];
+        return acc;
+      }
+    },
+    (null as unknown) as DocumentNode
+  );
 
   return {
-    typeDefs: gql`
-        extend type User {
-          audit: AuditEventsConnection @pgQuery(
-            source: ${embed(
-              (queryBuilder: QueryBuilder, args: AuditEventArgs) => {
-                return sql.fragment`
-                postgraphile_audit_plugin.get_audit_information(${queryBuilder.getTableAlias()}.audit_id, 'postgraphile_audit_plugin', 'users')
-            `;
-              }
-            )}
-          )
-        }
-      `,
+    typeDefs,
   };
 });
